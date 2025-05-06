08bcda89fca24fdff771b6e3a67193505f9f1812
Martin Grahovski <martingrahovski2004@gmail.com>
2025-05-06 11:37:18 +0300

    Implementing AuthService and Interceptor

---
+++ trendify.Client/src/app/models/account/auth-response-model.ts
@@ -0,0 +1,5 @@
+export interface AuthResponseModel {
+    token: string;
+    isSuccess: true;
+    message: string;
+  }
---
+++ trendify.Client/src/app/services/auth-service.ts
@@ -0,0 +1,68 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "../../environments/environment";
+import { jwtDecode } from "jwt-decode";
+import { map, Observable } from "rxjs";
+import { LoginModel } from "../models/account/login-model";
+import { AuthResponseModel } from "../models/account/auth-response-model";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthService {
+
+    apiUrl: string = environment.apiUrl;
+    private tokenKey = 'token';
+
+    constructor(private http: HttpClient) { }
+
+
+    login(data: LoginModel): Observable<AuthResponseModel> {
+        return this.http
+            .post<AuthResponseModel>(`${this.apiUrl}account/login`, data)
+            .pipe(
+                map((response) => {
+                    if (response.isSuccess) {
+                        localStorage.setItem(this.tokenKey, response.token);
+                    }
+                    return response;
+                })
+            );
+    }
+
+    getUserDetail = () => {
+        const token = this.getToken();
+        if (!token) return null;
+        const decodedToken: any = jwtDecode(token);
+        const userDetail = {
+            id: decodedToken.nameid,
+            fullName: decodedToken.name,
+            email: decodedToken.email,
+            roles: decodedToken.role || [],
+        };
+
+        return userDetail;
+    };
+
+    isLoggedIn = (): boolean => {
+        const token = this.getToken();
+        if (!token) return false;
+        return !this.isTokenExpired();
+    };
+
+    private isTokenExpired() {
+        const token = this.getToken();
+        if (!token) return true;
+        const decoded = jwtDecode(token);
+        const isTokenExpired = Date.now() >= decoded['exp']! * 1000;
+        if (isTokenExpired) this.logout();
+        return isTokenExpired;
+    }
+
+    logout = (): void => {
+        localStorage.removeItem(this.tokenKey);
+    };
+
+    getToken = (): string | null =>
+        localStorage.getItem(this.tokenKey) || '';
+}

fcb97a7d250533d13b949f4ac7800f07d387156e
Martin Grahovski <martingrahovski2004@gmail.com>
2025-05-07 20:40:34 +0300

    Adding Product Table and Repository

--- trendify.Server/Data/AppDbContext.cs
+++ trendify.Server/Data/AppDbContext.cs
@@ -8,7 +8,9 @@ namespace trendify.Server.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
+
         }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ trendify.Server/Migrations/20250507173301_Adding_Product_Table.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace trendify.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adding_Product_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    isOnSale = table.Column<bool>(type: "bit", nullable: false),
+                    isItNew = table.Column<bool>(type: "bit", nullable: false),
+                    isFeatured = table.Column<bool>(type: "bit", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShortDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}

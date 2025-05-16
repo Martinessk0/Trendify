7e251691d01fa6c68050bce012a79f32cbf7515f
Martin Grahovski <martingrahovski2004@gmail.com>
2025-05-17 02:54:41 +0300

    Reorder Product form dialog

--- trendify.Client/src/app/components/admin/products/product-form-dialog/product-form-dialog.component.ts
+++ trendify.Client/src/app/components/admin/products/product-form-dialog/product-form-dialog.component.ts
@@ -7,6 +7,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CategoryService } from '../../../../services/category-service';
+import { CategoryModel } from '../../../../models/category-model';
+import { NgFor } from '@angular/common';
 
 export interface ProductDialogData {
   title: string;
@@ -22,18 +25,22 @@ export interface ProductDialogData {
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    MatButtonModule],
+    MatButtonModule,
+    NgFor
+  ],
   templateUrl: './product-form-dialog.component.html',
   styleUrl: './product-form-dialog.component.scss'
 })
 export class ProductFormDialogComponent {
   form: FormGroup;
   action = FormActions;
+  categories: CategoryModel[] = [];
 
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<ProductFormDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ProductDialogData
+    @Inject(MAT_DIALOG_DATA) public data: ProductDialogData,
+    private categoryService: CategoryService
   ) {
     this.form = this.fb.group({
       name: [data.model.name, Validators.required],
@@ -44,6 +51,15 @@ export class ProductFormDialogComponent {
       isOnSale: [data.model.isOnSale ?? false],
       isItNew: [data.model.isItNew ?? false],
       isFeatured: [data.model.isFeatured ?? false],
+    });
+
+    this.loadCategories();
+  }
+
+  loadCategories() {
+    this.categoryService.getAllCategories().subscribe({
+      next: (categories) => (this.categories = categories),
+      error: (err) => console.error('Failed to load categories:', err),
     });
   }
 

a3c673a19eed3a03baa678a8876484192481ba17
Martin Grahovski <mgrahovski@technologica.com>
2025-05-19 16:12:49 +0300

    Redesign cart component

--- trendify.Client/src/app/components/public/cart/cart.component.ts
+++ trendify.Client/src/app/components/public/cart/cart.component.ts
@@ -3,12 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../../services/cart-service';
 import { ShoppingCartModel } from '../../../models/cart/shoppingCart-model';
 import { CartItemModel } from '../../../models/cart/cartItem-model';
+import { RouterLink } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-cart',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })

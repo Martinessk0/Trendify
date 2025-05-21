08b9e51f12a1d9f380a21891d1f30a5eb4e43974
Martin Grahovski <martingrahovski2004@gmail.com>
2025-05-21 11:58:14 +0300

    Editing Product Card Component

--- trendify.Client/src/app/components/shared/product-card/product-card.component.ts
+++ trendify.Client/src/app/components/shared/product-card/product-card.component.ts
@@ -4,6 +4,8 @@ import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ProductModel } from '../../../models/product-model';
 import { CategoryModel } from '../../../models/category-model';
+import Swal from 'sweetalert2';
+import { CartService } from '../../../services/cart-service';
 
 @Component({
   selector: 'app-product-card',
@@ -15,4 +17,29 @@ import { CategoryModel } from '../../../models/category-model';
 export class ProductCardComponent {
   @Input() product!: ProductModel;
   @Input() categories: CategoryModel[] = [];
+
+  constructor(private cartService: CartService){
+
+  }
+
+   addToCart(): void {
+    if (!this.product) {
+      Swal.fire('Error', 'Invalid product.', 'error');
+      return;
+    }
+
+    this.cartService.addItem({productId: +this.product.id,quantity: 1}).subscribe({
+      next: () => {
+        Swal.fire({
+          title: 'Added to Cart!',
+          text: `${this.product!.name} was added successfully.`,
+          icon: 'success',
+          confirmButtonText: 'OK'
+        });
+      },
+      error: () => {
+        Swal.fire('Failed', 'Could not add product to cart. Please try again.', 'error');
+      }
+    });
+  }
 }

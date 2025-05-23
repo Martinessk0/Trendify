bdfc3c45e5d1f2910288644565762b672d039f79
Martin Grahovski <mgrahovski@technologica.com>
2025-05-23 15:04:02 +0300

    Removing Footer from the admin menu

--- trendify.Client/src/app/app.component.ts
+++ trendify.Client/src/app/app.component.ts
@@ -1,22 +1,17 @@
-import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { FooterPageComponent } from "./components/public/footer-page/footer-page.component";
-
-interface Product {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  imageUrl: string;
-}
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule, FooterPageComponent],
+  imports: [RouterOutlet, FooterPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private router: Router) {}
+
+  get isAdmin(): boolean {
+    return this.router.url.includes('/admin');
+  }
 }
